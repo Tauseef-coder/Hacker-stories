7e5692ed77f803de2e026eaa19fc788118d4871c
Tauseef-Coder <lm4cg33k@aol.com>
2021-03-20 01:54:41 -0400

    fundamentals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from "react";
-function getTitle(title){
-  return title;
-
-
-};
-
-
-
-function App(){
-  const title = "React";
-  // do something in between
-  return (
-    <div> 
-      <h1> Hello  {getTitle("React")}</h1> 
-
-      <label htmlFor="search"> Search : </label>
+const list = [
+{
+  title: "React",
+  url: "https://reactjs.org/",
+  author: "jordan walke",
+  num_comments: 3,
+  points : 4,
+  objectID : 0,
+},
+{
+  title: "Redux",
+  Url: "https://redux.js.org/",
+  author: "Dan Abramov, Andrew Clark",
+  num_comments :2,
+  points : 5,
+  objectID : 1,
+},
+];
+function App() { 
+  return(
+    <div>
+      <h1>My Hacker Stories</h1>
+      
+      <label htmlFor="search">Search : </label>
       <input id="search" type="text"/>
-    </div>
+      <hr />
 
-  );
-}
+      {list.map(function(item) {
+        return 
+          <div key={item.objectId}>
+            <span>
+      <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            {item.tilte};
+            </div>
+      })}
+
+      {/* render the list here*/}
+    </div>
+  )
+ }
 
 export default App;
